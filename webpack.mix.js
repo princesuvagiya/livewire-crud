97c68d409dc134929fab03818af00d649ea6472f
prince <prince.suhagiya@shunyavkash.com>
2023-01-16 17:18:35 +0530

    laravel mix add

---
+++ webpack.mix.js
@@ -0,0 +1,13 @@
+// webpack.mix.js
+
+const mix = require('laravel-mix');
+
+
+mix.js('resources/js/app.js', 'js')
+
+   .sass('resources/sass/app.scss', 'css')
+
+   .setPublicPath('public');
+
+
+
